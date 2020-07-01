d62ad5ef504cbf9f24d8eaf7de3262f3a07d8974
Cassio Murilo <cmurilo.san@gmail.com>
2020-07-01 00:10:04 -0300

    Aula 01 - Simulando comportamentos com Mock Objects

---
+++ test/br/com/caelum/leilao/servico/EncerradorDeLeilaoTest.java
@@ -0,0 +1,43 @@
+package br.com.caelum.leilao.servico;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+import java.util.Arrays;
+import java.util.Calendar;
+
+import org.junit.Test;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import br.com.caelum.leilao.builder.CriadorDeLeilao;
+import br.com.caelum.leilao.dominio.Leilao;
+import br.com.caelum.leilao.infra.dao.LeilaoDao;
+import br.com.caelum.leilao.infra.dao.LeilaoDaoFalso;
+
+public class EncerradorDeLeilaoTest {
+	
+	@Test
+	public void deveEncerrarLeiloesQueComecaramUmaSemanaAntes() {
+		
+		Calendar antiga = Calendar.getInstance();
+		antiga.set(1999, 1, 20);
+		
+		Leilao leilao1 = new CriadorDeLeilao().para("TV").naData(antiga).constroi();
+		Leilao leilao2 = new CriadorDeLeilao().para("Celular").naData(antiga).constroi();
+		List<Leilao> leiloesAntigos = Arrays.asList(leilao1, leilao2);
+		
+		LeilaoDao daoFalso = mock(LeilaoDao.class);
+		
+		when(daoFalso.correntes()).thenReturn(leiloesAntigos);
+		
+		EncerradorDeLeilao encerrador = new EncerradorDeLeilao(daoFalso);
+		encerrador.encerra();
+		
+		
+		assertEquals(2, encerrador.getTotalEncerrados());
+		assertTrue(leilao1.isEncerrado());
+		assertTrue(leilao2.isEncerrado());
+	}
+
+}
